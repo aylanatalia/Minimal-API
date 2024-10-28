e9aeee4da3209defa3688c482385bcb5f60723b3
Ayla Natalia Martins Ferreira <aylantalia08@hotmail.com>
2024-10-28 10:40:24 -0300

    commit incial

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using Minimal;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@test.com" && loginDTO.Senha == "123456"){
+        return Results.Ok("Login com sucesso");
+    }else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
